eb2ee35f49926f67ef8217bbc19a13651edb551f
BakPeter <bak.pet@gmail.com>
2021-09-26 11:59:40 +0300

    3_build-a-model-db-first

---
+++ EntityFramewokInDepth/PlutoDbFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoDbFirst
+{
+    public enum MyLevelEnum : byte
+    {
+        FIRST = 1,
+        SECOND = 2,
+        THIRD = 3
+    }
+
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            var contextPluto = new PlutoDbContext();
+            using(var courses= contextPluto.GetCourses())
+            {
+                foreach(var course in courses)
+                {
+                    Console.WriteLine(course.Title);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
